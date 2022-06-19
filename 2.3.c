2ed1a2596996ea40e6fbbded6e62e7040eaead91
Tuan Ain <2020865508@student.uitm.edu.my>
2022-06-20 07:16:22 +0800

    create 2.3

---
+++ 2.3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void childTask()
+
+{
+	printf("salam, i'm a child");
+}
+void parentTask()
+{
+	printf("salam i'm their parents");
+}
+int main (void)
+{
+	pid_t pid = fork();
+	if (pid == fork();
+	childTask();
+	exit(EXIT_SUCCES);
+}
+	else if(pid > 0)
+{
+	wait(NULL);
+	parentTask();
+}
+	else
+{
+	printf("unable to create child process\n");
+}
+	return EXIT_SUCCES;
+}

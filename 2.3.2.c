55f62ff4836d0b8976bcf8cccab1573170ad5f3c
Tuan Ain <2020865508@student.uitm.edu.my>
2022-06-20 07:38:48 +0800

    create 2.3.2.c

---
+++ 2.3.2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+	for(int i=1; i<13; i++)
+{
+	pid_t pid = fork();
+	if(pid ==0)
+{
+	printf("child process => PPID=%d, PID=%d\n", getppid(), getpid());
+	exit(0);
+}
+	else
+{
+	printf("parent process => PID=%d\n"), getpid());
+	printf("waiting for child process to finish...\n");
+	wait(NULL);
+	printf("child process finished.\n");
+}
+}
+	return EXIT_SUCCESS;
+}

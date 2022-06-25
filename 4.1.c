1b57be5136ddd9ca984b9f3449e87803b0145876
Tuan Ain <2020865508@student.uitm.edu.my>
2022-06-26 02:17:50 +0800

    edit code 4.1

--- 4.1.c
+++ 4.1.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>	//inet_addr
+#include<string.h>
 
 int main(int argc , char *argv[])
 {
 	int socket_desc;
 	struct sockaddr_in server;
+	char *message , server_reply[2000];
 	
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -25,6 +27,24 @@ int main(int argc , char *argv[])
 		return 1;
 	}
 	
-	puts("Connected");
+	puts("Connected \n");
+
+	//Send some data
+	message = "connect";
+	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+	puts("Data Send\n");
+                    //Receive a reply from the server
+	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+	{
+		puts("recv failed");
+	}
+	puts("Reply received\n");
+	puts(server_reply);
+
 	return 0;
 }
+
